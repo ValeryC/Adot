9ccef73d19f87c185c74bf9590450db4e45b9856
Valery CHIN <chinvalery@gmail.com>
2022-07-16 19:16:32 +0200

    [error]configure validation with yup

--- src/utils/formRules.ts
+++ src/utils/formRules.ts
@@ -1,17 +1,14 @@
-const FormRules = {
-  destination: {
-    required: {
-      value: true,
-      message: 'destination name is required',
-    },
-  },
-  adress: {
-    required: {
-      value: true,
-      message: 'adress is required'
-    }
-  }
+import * as yup from 'yup';
 
-};
+const schema = yup.object().shape({
+  destination: yup.string().required('*destination requise'),
+  adress: yup.string().required('*adresse requise'),
+  link: yup.string().required('*lien image requis'),
+  population: yup.number().integer().typeError('*nombre requis').positive('*doit être positif'),
+  hotel: yup.number().integer().typeError('*nombre requis').positive('*doit être positif'),
+  income: yup.number().integer().typeError('*nombre requis').positive('*doit être positif'),
+  area: yup.number().integer().typeError('*nombre requis').positive('*doit être positif'),
+  toggle: yup.boolean()
+})
 
-export default FormRules;
+export default schema;
