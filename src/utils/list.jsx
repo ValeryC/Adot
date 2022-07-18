1eee654b7b392a1bc94b03c283e0b9c6528254fc
Valery CHIN <chinvalery@gmail.com>
2022-07-18 19:24:07 +0200

    [sample]create 4 samples of destination

---
+++ src/utils/list.jsx
@@ -0,0 +1,45 @@
+import FormProps from "../models/formValidation";
+
+const FewDestinations: FormProps = [
+  {
+    destination: "Paris",
+    adress: "Rue du Champs de mars",
+    link: "https://www.parisselectbook.com/wp-content/uploads/2021/05/paris-tour-eiffel-ps.jpeg",
+    population: 2,
+    hotel: 12000,
+    income: 70000,
+    area: 1000,
+    toggle: false,
+  },
+  {
+    destination: "London",
+    adress: "SW1A 0AA, United Kingdom",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Dusky_Big_Ben_%28180813845%29.jpeg/1600px-Dusky_Big_Ben_%28180813845%29.jpeg?20180818005736",
+    population: 9,
+    hotel: 10000,
+    income: 90000,
+    area: 1500,
+    toggle: true,
+  },
+  {
+    destination: "New York",
+    adress: "avenue brodway",
+    link: "https://agencesmvoyages.fr/sites/agencemvoyages/files/styles/carrousel/public/image/nyc.jpeg?itok=u_pnYPYh",
+    population: 2,
+    hotel: 12000,
+    income: 70000,
+    area: 1000,
+    toggle: false,
+  },
+  {
+    destination: "Tokyo",
+    adress: "Asada Mesh Co., Ltd. Hideaki HAYASHI",
+    link: "https://i0.wp.com/www.japan-kudasai.com/wp-content/uploads/Seaside-top-observatory-Tokyo-14.jpg?w=1080&ssl=1",
+    population: 10,
+    hotel: 24000,
+    income: 80000,
+    area: 1200,
+    toggle: false,
+  },
+];
+export default FewDestinations;
