9848643a47c7cb6972be2cbe56884d316dfe857c
Valery CHIN <chinvalery@gmail.com>
2022-07-18 19:21:56 +0200

    [format]yarn format:fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,23 @@
-
-import { Button } from 'react-bootstrap'
-import NiceModal from '@ebay/nice-modal-react';
-import CreateDestinationModal from './CreateDestinationModal';
+import { Button } from "react-bootstrap";
+import NiceModal from "@ebay/nice-modal-react";
+import CreateDestinationModal from "./CreateDestinationModal";
 
 const Header: React.FC<{}> = () => {
   return (
     <div className="container-header">
-      <h2>
-        Destinations
-      </h2>
+      <h2>Destinations</h2>
       <Button
         className="button"
         onClick={() => {
-          NiceModal.show(NiceModal.create(CreateDestinationModal),
-            { title: 'Ajouter une nouvelle destination' }
-          )
+          NiceModal.show(NiceModal.create(CreateDestinationModal), {
+            title: "Ajouter une nouvelle destination",
+          });
         }}
       >
         + &ensp;Ajouter
       </Button>
     </div>
-  )
+  );
+};
 
-}
-
-export default Header
+export default Header;
