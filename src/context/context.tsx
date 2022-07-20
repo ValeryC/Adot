379654d9085631012a7b1a5121be906906f12f50
Valery CHIN <chinvalery@gmail.com>
2022-07-20 08:07:47 +0200

    [context]create context to get information before using localstorage

---
+++ src/context/context.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, createContext } from 'react';
+import FewDestinations from '../utils/list'
+import { FormProps } from "../models/formValidation";
+
+export const dataContext = createContext({
+  destinations: FewDestinations,
+  setDestinations: (props: FormProps) => { },
+})
+export const DestinationContext = ({ children }: any) => {
+  const [destinations, setDestinations] = useState<FormProps>(
+    // checkdata retrieve value in localstorage
+    FewDestinations
+  )
+  useEffect(() => {
+    localStorage.setItem(
+      "items",
+      JSON.stringify([destinations])
+    );
+  }, [destinations]);
+
+  return (
+    <dataContext.Provider value={{ destinations, setDestinations }}>
+      {children}
+    </dataContext.Provider>
+  )
+}
