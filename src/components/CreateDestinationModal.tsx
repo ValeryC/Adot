05ead2e6b25c0d9aa0633587b7ce9edac9bfe2b8
Valery CHIN <chinvalery@gmail.com>
2022-07-21 08:14:05 +0200

    [update]add popover for form modal

--- src/components/CreateDestinationModal.tsx
+++ src/components/CreateDestinationModal.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Modal, Form, Button, Col, Row } from "react-bootstrap";
+import {
+  Modal,
+  Form,
+  Button,
+  Col,
+  Row,
+  Popover,
+  OverlayTrigger,
+} from "react-bootstrap";
 import { bootstrapDialog, useModal } from "@ebay/nice-modal-react";
 import schema from "../utils/formRules";
 import {
@@ -43,54 +51,153 @@ const CreateDestinationModal = ({ title }: CreateDestinationModalProps) => {
           <Row>
             <Col>
               <p> {errors?.destination?.message}</p>
-              <Form.Control
-                placeholder="Nom de la destination"
-                {...register("destination")}
-              />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="right"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Nom de la destination</Popover.Header>
+                    <Popover.Body>
+                      Veuillez entrer un nom de destination type "Madrid"
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Nom de la destination"
+                  {...register("destination")}
+                />
+              </OverlayTrigger>
             </Col>
           </Row>
           <Row>
             <Col>
               <p> {errors?.adress?.message}</p>
-              <Form.Control
-                placeholder="Adresse"
-                autoFocus
-                {...register("adress")}
-              />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="right"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Adresse</Popover.Header>
+                    <Popover.Body>
+                      Veuillez entrer une adresse type "Avenue des champs"
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Adresse"
+                  autoFocus
+                  {...register("adress")}
+                />
+              </OverlayTrigger>
             </Col>
           </Row>
           <Row>
             <Col>
               <p> {errors?.link?.message}</p>
-              <Form.Control
-                placeholder="Lien de l'image"
-                {...register("link")}
-              />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="right"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Lien de votre image</Popover.Header>
+                    <Popover.Body>
+                      Veuillez entrer une adresse image de votre choix type
+                      "https://images.pexels.com/photos/290595/pexels-photo-290595.jpeg?cs=srgb&dl=pexels-pixabay-290595.jpg&fm=jpg"
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Lien de l'image"
+                  {...register("link")}
+                />
+              </OverlayTrigger>
             </Col>
           </Row>
           <Row>
             <Col md={3}>
               <p> {errors?.population?.message}</p>
-              <Form.Control
-                placeholder="Nb Habitants"
-                {...register("population")}
-              />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="bottom"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Nombre d'habitants</Popover.Header>
+                    <Popover.Body>
+                      Le nombre d'habitant est en million. Si vous mettez le
+                      chiffre 1.2 cela validera 1.2 Million.
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Nb Habitants"
+                  {...register("population")}
+                />
+              </OverlayTrigger>
             </Col>
             <Col md={3}>
               <p> {errors?.hotel?.message}</p>
-              <Form.Control placeholder="Nb Hôtels" {...register("hotel")} />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="bottom"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Nombre d'hôtels</Popover.Header>
+                    <Popover.Body>
+                      Veuiller saisir un nombre entier qui sera exprimé en
+                      nombre d'hôtel.
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control placeholder="Nb Hôtels" {...register("hotel")} />
+              </OverlayTrigger>
             </Col>
             <Col md={3}>
               <p> {errors?.income?.message}</p>
-              <Form.Control placeholder="Revenu Moy" {...register("income")} />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="bottom"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Revenu Moyen</Popover.Header>
+                    <Popover.Body>
+                      Veuiller saisir un nombre entier qui sera exprimé en
+                      euros.
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Revenu Moy"
+                  {...register("income")}
+                />
+              </OverlayTrigger>
             </Col>
             <Col md={3}>
               <p> {errors?.area?.message}</p>
-              <Form.Control
-                placeholder="Superficie"
-                data-testid="area"
-                {...register("area")}
-              />
+              <OverlayTrigger
+                trigger={["hover", "focus"]}
+                placement="bottom"
+                overlay={
+                  <Popover>
+                    <Popover.Header>Superficie</Popover.Header>
+                    <Popover.Body>
+                      La superficie sera en km2. Si vous saissisez 2.4 cela
+                      validera 2.4 km2.
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <Form.Control
+                  placeholder="Superficie"
+                  data-testid="area"
+                  {...register("area")}
+                />
+              </OverlayTrigger>
             </Col>
           </Row>
           <Row>
