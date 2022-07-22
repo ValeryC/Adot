243dd66a2a80cc1297896ee4867a769ba293df3d
Valery <chinvalery@gmail.com>
2022-07-22 15:49:04 +0200

    [format]yarn format:fix

--- src/components/DeleteDestinationModal.tsx
+++ src/components/DeleteDestinationModal.tsx
@@ -3,20 +3,23 @@ import { Modal, Button } from "react-bootstrap";
 
 type DeleteDestinationModalProps = {
   title: string;
-  index:number;
+  index: number;
 };
 
-const DeleteDestinationModal = ({ title,index }: DeleteDestinationModalProps) => {
+const DeleteDestinationModal = ({
+  title,
+  index,
+}: DeleteDestinationModalProps) => {
   const modal = useModal();
 
-  const deleteItem = () => { 
-    let items = JSON.parse(localStorage.getItem('items')as string)
-    var itemIndex = index // index of item to be removed
-    items.splice(itemIndex, 1)
-    localStorage.setItem('items', JSON.stringify(items));
+  const deleteItem = () => {
+    let items = JSON.parse(localStorage.getItem("items") as string);
+    let itemIndex = index; // index of item to be removed
+    items.splice(itemIndex, 1);
+    localStorage.setItem("items", JSON.stringify(items));
     window.location.href = "http://localhost:3000/";
-    modal.hide() 
-  }
+    modal.hide();
+  };
   return (
     <Modal className="delete_modal" centered {...bootstrapDialog(modal)}>
       <Modal.Header>
@@ -25,22 +28,22 @@ const DeleteDestinationModal = ({ title,index }: DeleteDestinationModalProps) =>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-          <Button
-            aria-label="btn-cancel"
-            variant="secondary"
-            onClick={modal.hide}
-          >
-            Cancel
-          </Button>
-          <Button
-            aria-label="btn-submit-form"
-            className="confirm"
-            variant="success"
-            onClick={()=>deleteItem()}
-          >
-            Confirm
-          </Button>
-        </Modal.Body>
+        <Button
+          aria-label="btn-cancel"
+          variant="secondary"
+          onClick={modal.hide}
+        >
+          Cancel
+        </Button>
+        <Button
+          aria-label="btn-submit-form"
+          className="confirm"
+          variant="success"
+          onClick={() => deleteItem()}
+        >
+          Confirm
+        </Button>
+      </Modal.Body>
     </Modal>
   );
 };
