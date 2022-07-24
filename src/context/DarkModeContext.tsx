0a5f76841e3694d00446a0c4cfd4e7af8b2705f8
Valery <chinvalery@gmail.com>
2022-07-24 23:21:34 +0200

    [data]put value in localstorage

--- src/context/DarkModeContext.tsx
+++ src/context/DarkModeContext.tsx
@@ -9,9 +9,17 @@ export const DarkModeContext = createContext<DarkModeContextProps>(
 
 export const DarkModeProvider = ({ children }: PropsWithChildren) => {
   const [darkMode, setDarkMode] = useState<Theme>("light");
-
+  if (localStorage.getItem("dark") === null) {
+  localStorage.setItem("dark", JSON.stringify(darkMode))
+  }
   const toggleDarkMode = () => {
     setDarkMode(darkMode === "light" ? "dark" : "light");
+    if(darkMode === "dark") {
+      localStorage.setItem("dark", JSON.stringify("light"))
+    }
+    else if(darkMode === "light") {
+      localStorage.setItem("dark", JSON.stringify("dark"))
+    }
   };
 
   const color = darkMode === "light" ? "#333" : "#FFF";
